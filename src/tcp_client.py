0a83e0a7b52a29a33512a406b123ac9d56d925e1
guanlunlu <guanlunlu@gapp.nthu.edu.tw>
2022-05-17 03:51:07 +0800

    add obstacle to map topic

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -7,22 +7,31 @@ import json
 from tf.transformations import euler_from_quaternion
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from obstacle_detector.msg import Obstacles
+from obstacle_detector.msg import CircleObstacle
 from std_msgs.msg import Int32
 
 class tcp_client:
     def __init__(self):
-        self.HOST = '192.168.50.195'
+        self.HOST = '192.168.50.2'
         self.PORT = 7001
-        self.HOST = rospy.get_param("tcp_host", '192.168.50.195')
+        self.HOST = rospy.get_param("tcp_host", '192.168.50.2')
         self.PORT = rospy.get_param("tcp_port", 7001)
 
         self.robot_pose = [0,0,0]
         self.start_running = 0
+        
         self.pose_sub = rospy.Subscriber("ekf_pose", PoseWithCovarianceStamped, self.poseCallback)
         self.start_sub = rospy.Subscriber("start_running", Int32, self.startCallback)
 
         self.ally_pub = rospy.Publisher("tera_pose", PoseWithCovarianceStamped, queue_size=10)
         self.score_pub = rospy.Publisher("add_Point", Int32, queue_size= 10)
+
+        self.obstacle_list = []
+        self.ally_obstacle_list = []
+        self.obs_sub = rospy.Subscriber("obstacles_to_map", Obstacles, self.obsCallback)
+        self.ally_obs_pub = rospy.Publisher("ally_obstacle_to_map", Obstacles, queue_size=10)
+
         self.clientInitialize()
 
     def clientInitialize(self):
@@ -31,12 +40,13 @@ class tcp_client:
             try:
                 while True and not rospy.is_shutdown():
                     # send current pose to ally
-                    send_data = []
-                    send_data = self.robot_pose.copy()
-                    send_data.append(self.start_running)
-                    send_data = str(send_data)
-                    socket_.send(send_data.encode())
-                    rospy.loginfo_throttle(0.1, '[Pico] send: ' + send_data)
+                    # send_data = []
+                    # send_data = self.robot_pose.copy()
+                    # send_data.append(self.start_running)
+                    # send_data = str(send_data)
+                    # socket_.send(send_data.encode())
+                    # rospy.loginfo_throttle(0.1, '[Pico] send: ' + send_data)
+                    socket_.send(self.messageEncode())
 
                     # receive ally's current pose
                     indata = socket_.recv(1024)
@@ -44,13 +54,15 @@ class tcp_client:
                         socket_.close()
                         print('server closed connection.')
                         break
-                    recv_data = indata.decode()
-                    rospy.loginfo_throttle(0.1, '[Pico] recv: ' + recv_data)
-                    recv_data = json.loads(recv_data)
-                    self.allyPosePublish(recv_data)
-                    self.allyScorePublish(recv_data)
+                    
+                    # recv_data = indata.decode()
+                    # rospy.loginfo_throttle(0.1, '[Pico] recv: ' + recv_data)
+                    # recv_data = json.loads(recv_data)
+                    # self.allyPosePublish(recv_data)
+                    # self.allyScorePublish(recv_data)
 
-                    rospy.loginfo_throttle(0.1, "---")
+                    # rospy.loginfo_throttle(0.1, "---")
+                    self.messageDecode(indata)
                     rospy.Rate(30).sleep()
 
             except socket.error:
@@ -58,6 +70,38 @@ class tcp_client:
                 rospy.sleep(0.5)
                 self.initConnection
 
+    def messageEncode(self):
+        send_data = []
+        send_data = self.robot_pose.copy()
+        send_data.append(self.start_running)
+
+        for obs in self.obstacle_list:
+            send_data.append(round(obs[0],4))
+            send_data.append(round(obs[1],4))
+
+        send_data = str(send_data)
+        rospy.loginfo_throttle(0.1,'[Pico] send: ' + send_data)
+        rospy.loginfo_throttle(0.1,"---")
+        msg = send_data.encode()
+        return msg
+
+    def messageDecode(self, raw_data):
+        recv_data = raw_data.decode()
+        rospy.loginfo_throttle(0.1,'[Pico] recv: ' + recv_data)
+        recv_data = json.loads(recv_data)
+        if len(recv_data) != 0:
+            self.allyPosePublish(recv_data)
+            self.allyScorePublish(recv_data)
+
+            obs_list = []
+            obs = []
+            for i in range(len(recv_data)):
+                if i > 3 and i%2 == 0:
+                    obs_list.append([recv_data[i], recv_data[i+1]])
+
+            self.ally_obstacle_list = obs_list.copy()
+            self.allyObstaclePublish()
+        
     def initConnection(self, host, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try :
@@ -66,12 +110,19 @@ class tcp_client:
             pass
         return sock
 
+    def obsCallback(self, data):
+        del self.obstacle_list[:]
+        for obs in data.circles:
+            x = obs.center.x
+            y = obs.center.y
+            self.obstacle_list.append([x, y])
+
     def poseCallback(self, data):
         x = data.pose.pose.position.x
         y = data.pose.pose.position.y
         quat = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]
         roll, pitch, yaw = euler_from_quaternion(quat)
-        self.robot_pose = [x, y, yaw]
+        self.robot_pose = [round(x,4), round(y,4), round(yaw,4)]
 
     def startCallback(self, data):
         self.start_running = data.data
@@ -90,9 +141,23 @@ class tcp_client:
         self.ally_pub.publish(msg)
 
     def allyScorePublish(self, recv_data):
-        msg = Int32()
-        msg.data = recv_data[3]
-        self.score_pub.publish(msg)
+        if recv_data[3] != -1:
+            msg = Int32()
+            msg.data = recv_data[3]
+            self.score_pub.publish(msg)
+
+    def allyObstaclePublish(self):
+        msg = Obstacles()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "map"
+        for i in self.ally_obstacle_list:
+            cir_obs = CircleObstacle()
+            cir_obs.x = i[0]
+            cir_obs.y = i[1]
+            msg.obstacles.append(cir_obs)
+        self.ally_obs_pub.publish(msg)
+        del self.ally_obstacle_list[:]
+        
 
 if __name__ == "__main__":
     rospy.init_node("tcp_client", anonymous = True)
--- src/tcp_server.py
+++ src/tcp_server.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
 
 import rospy
+import numpy as np
 import socket
 import json
 
 from tf.transformations import euler_from_quaternion
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from obstacle_detector.msg import Obstacles
+from obstacle_detector.msg import CircleObstacle
 from std_msgs.msg import Int32
 from std_srvs.srv import Empty
 
 class tcp_server:
     def __init__(self) -> None:
-        self.HOST = '192.168.50.195'
+        self.HOST = '192.168.50.2'
         self.PORT = 7001
-        self.HOST = rospy.get_param("tcp_host", '192.168.50.195')
+        self.HOST = rospy.get_param("tcp_host", '192.168.50.2')
         self.PORT = rospy.get_param("tcp_port", 7001)
 
         self.robot_pose = [0,0,0]
         self.score = 0
+        self.if_tera_started = False
+        self.obstacle_list = []
+        self.ally_obstacle_list = []
+
         self.pose_sub = rospy.Subscriber("ekf_pose", PoseWithCovarianceStamped, self.poseCallback)
         self.score_sub = rospy.Subscriber("add_Point", Int32, self.scoreCallback)
-        self.if_tera_started = False
-
         self.ally_pub = rospy.Publisher("pico_pose", PoseWithCovarianceStamped, queue_size=10)
+
+        self.obs_sub = rospy.Subscriber("obstacle_array", ObstacleArrayMsg, self.obsCallback)
+        self.ally_obs_pub = rospy.Publisher("ally_obstacle_array", ObstacleArrayMsg, queue_size=10)
+
+
         self.serverInitialize()
 
     def serverInitialize(self):
@@ -52,22 +62,57 @@ class tcp_server:
                     conn.close()
                     rospy.logerr_throttle(0.1, 'Lost connection from Client')
                     break
-                recv_data = indata.decode()
-                rospy.loginfo_throttle(0.1,'[Tera] recv: ' + recv_data)
-                recv_data = json.loads(recv_data)
-                self.allyPublish(recv_data)
-                self.triggerAlly(recv_data)
+
+                # recv_data = indata.decode()
+                # rospy.loginfo_throttle(0.1,'[Tera] recv: ' + recv_data)
+                # recv_data = json.loads(recv_data)
+                # self.allyPublish(recv_data)
+                # self.triggerAlly(recv_data)
+                self.messageDecode(indata)
 
                 # send current pose to ally
-                send_data = []
-                send_data = self.robot_pose.copy()
-                send_data.append(self.score)
-                send_data = str(send_data)
-                rospy.loginfo_throttle(0.1,'[Tera] send: ' + send_data)
-                rospy.loginfo_throttle(0.1,"---")
+                # send_data = []
+                # send_data = self.robot_pose.copy()
+                # send_data.append(self.score)
+                # send_data = str(send_data)
+                # rospy.loginfo_throttle(0.1,'[Tera] send: ' + send_data)
+                # rospy.loginfo_throttle(0.1,"---")
+                # conn.send(send_data.encode())
+                conn.send(self.messageEncode())
 
-                conn.send(send_data.encode())
+    def messageEncode(self):
+        send_data = []
+        send_data = self.robot_pose.copy()
+        send_data.append(self.score)
 
+        for obs in self.obstacle_list:
+            send_data.append(round(obs[0],4))
+            send_data.append(round(obs[1],4))
+
+        send_data = str(send_data)
+        rospy.loginfo_throttle(0.1,'[Tera] send: ' + send_data)
+        rospy.loginfo_throttle(0.1,"---")
+        msg = send_data.encode()
+        return msg
+
+    def messageDecode(self, raw_data):
+        recv_data = raw_data.decode()
+        rospy.loginfo_throttle(0.1,'[Tera] recv: ' + recv_data)
+        recv_data = json.loads(recv_data)
+        if len(recv_data) != 0:
+            self.allyPublish(recv_data)
+            self.triggerAlly(recv_data)
+
+            obs_list = []
+            obs = []
+            
+            for i in range(len(recv_data)):
+                if i > 3 and i%2 == 0:
+                    obs_list.append([recv_data[i], recv_data[i+1]])
+
+            self.ally_obstacle_list = obs_list.copy()
+            self.allyObstaclePublish()
+        
     def stateReset(self):
         self.if_tera_started = False
 
@@ -76,10 +121,18 @@ class tcp_server:
         y = data.pose.pose.position.y
         quat = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]
         roll, pitch, yaw = euler_from_quaternion(quat)
-        self.robot_pose = [x, y, yaw]
+        self.robot_pose = [round(x,4), round(y,4), round(yaw,4)]
 
     def scoreCallback(self, data):
         self.score = data.data
+        self.score = -1
+
+    def obsCallback(self, data):
+        del self.obstacle_list[:]
+        for obs in data.circles:
+            x = obs.center.x
+            y = obs.center.y
+            self.obstacle_list.append([x, y])
 
     def allyPublish(self, ally_pose):
         msg = PoseWithCovarianceStamped()
@@ -93,6 +146,18 @@ class tcp_server:
         msg.pose.pose.orientation.z = quaternion[2]
         msg.pose.pose.orientation.w = quaternion[3]
         self.ally_pub.publish(msg)
+
+    def allyObstaclePublish(self):
+        msg = Obstacles()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "map"
+        for i in self.ally_obstacle_list:
+            cir_obs = CircleObstacle()
+            cir_obs.x = i[0]
+            cir_obs.y = i[1]
+            msg.obstacles.append(cir_obs)
+        self.ally_obs_pub.publish(msg)
+        del self.ally_obstacle_list[:]
 
     def triggerAlly(self, data):
         if data[3] == 1 and self.if_tera_started == False:
